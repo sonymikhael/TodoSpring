ba2afcdc504d58cb4593d1b60f83c4f6fa15135b
Sony Fampulme <sonymikhael07@gmail.com>
2020-05-06 21:57:03 +0800

    initial commit

---
+++ src/test/java/com/smsf/Todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smsf.Todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
